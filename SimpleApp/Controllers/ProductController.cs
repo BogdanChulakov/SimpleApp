97a738eb182bec20fca420c830cca69eea74d278
Bogdan Chulakov <49172210+BogdanChulakov@users.noreply.github.com>
2021-03-21 03:47:53 +0200

    Add Product

---
+++ SimpleApp/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Models.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync(InputProductViewModel input)
+        {
+
+
+            return this.Json(input);
+        }
+    }
+}
---
+++ SimpleApp/Models/Product/InputProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Models.Product
+{
+    public class InputProductViewModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+    }
+}
